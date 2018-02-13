739192f72659afbb71eb9c5ae80c65b128c94aa6
tysonparks <typarks@gmail.com>
2018-02-13 08:53:54 -0800

    Init repo for Boxes

---
+++ Boxes/Boxes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Boxes
+//
+//  Created by Tyson Parks on 2/13/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
