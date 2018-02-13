a0df36472d21bb9f5225e3776c85bdd83f341b16
tysonparks <typarks@gmail.com>
2018-02-13 13:10:06 -0800

    In main.m instanced two Boxes initialized with dimensions, logged their volumes, and logged their fit.

--- Boxes/Boxes/main.m
+++ Boxes/Boxes/main.m
@@ -7,12 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Box.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
+        //initialize 2 boxes
+        Box *bigBox = [[Box alloc]initWithHeight:2 Width:3 Length:5];
+        Box *smallBox = [[Box alloc]initWithHeight:1 Width:2 Length:3];
         
-    
-    }
-    return 0;
-}
+        //log their volumes
+        NSLog(@"bigBox has a volume of %.f", bigBox.volumeOfBox);
+        NSLog(@"smallBox has a volume of %.f", smallBox.volumeOfBox);
+        
+        //find out how many boxes can fit in the other
+        float howManyCanFit = [bigBox howManyOfFirstBox:bigBox CanFitInSecondBox:smallBox];
+        NSLog(@"%.f boxes can fit into each other",howManyCanFit);
+         
+         }
+         return 0;
+         }
